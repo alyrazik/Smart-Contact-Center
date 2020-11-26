4c94355178a0fd7ed25516917d567a01f6b999f3
Aly <44007982+alyrazik@users.noreply.github.com>
2020-11-26 23:29:16 +0200

    I have a fear of commitment

---
+++ bin/test.py
@@ -0,0 +1,37 @@
+"""
+Testing code
+"""
+
+# Imports
+import plac
+import os
+import logging
+import sys
+import numpy
+import pandas
+
+
+# Import helper functions
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
+from time import time
+from datetime import datetime
+
+
+
+# setup logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger('Regression logger')
+
+def main():
+    try:
+        #do what you want
+        pass
+
+    except Exception:
+        logger.exception('Exception occured in running test function')
+
+
+if __name__ == '__main__':
+    plac.call(main)
