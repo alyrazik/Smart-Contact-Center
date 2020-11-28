d8f139bf5974cf2f431591db588b8377d8a18fe7
Aly <44007982+alyrazik@users.noreply.github.com>
2020-11-28 17:14:47 +0200

    implement facebook scraper for posts from a group

--- SCC/scrapers/facebook.py
+++ SCC/scrapers/facebook.py
@@ -2,8 +2,6 @@ import pandas as pd
 from facebook_scraper import get_posts
 
 
-SEARCH_LIMIT = 10
-
 def get_fb_posts(group, pages):
     posts = []
     for post in get_posts(group=group, pages=pages): # group ID for don't shop here group
